7645fe21cac75207767d9a0d9826813671fdb21a
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-03 08:45:06 -0600

    add accessors and mutators for ages and name

--- snap-magic-method.php
+++ snap-magic-method.php
@@ -13,4 +13,59 @@ class Person {
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getName() {
+		return ($this->name);
+	}
+
+	/**
+	 * mutator for name
+	 * @param $newName
+	 */
+	public function setName($newName): void {
+		// verify the  name is secure
+		$newName = trim($newName);
+		$newName = filter_var($newName, FILTER_SANITIZE_URL);
+		if(empty($newName) === true) {
+			throw(new \InvalidArgumentException("Name is empty or insecure. "));
+		}
+
+		// verify the name will fit in the database
+		if(strlen($newName) > 64) {
+			throw(new \RangeException("Your Name is too long. Limit 64 characters."));
+		}
+
+		// store the name
+		$this->name = $newName;
+	}
+
+	/**
+	 * accessor for age
+	* @return mixed
+	 */
+	public function getAge() {
+		return ($this->age);
+	}
+
+
+	public function setAge( int $newAge): int {
+		$this->age = $newAge;
+
+		//verifys that the age is a positive number
+		if($newAge < 0) {
+			throw (new \RangeException("Age cannot be less than zero"));
+		} else($newAge > 0 and <=18) {
+			return ("hello caleb")
+		}elseif ($newAge > 118){
+			return ("hi deepdivedylan")
+		}
+
+		//store the value if it passes validation
+		$this->age = $newAge;
+	}
+
+
 }
